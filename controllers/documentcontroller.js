b4b4496a9f4650f75691c7822d24b63a3c7b314e
paras1478 <pankaj147878@gmail.com>
2026-02-09 17:09:51 +0530

    fix: Linux case-sensitive import for document model

--- controllers/documentcontroller.js
+++ controllers/documentcontroller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import fs from "fs";
 import path from "path";
 
-import Document from "../models/Document.js";
+import Document from "../models/document.js";
 import Flashcard from "../models/Flashcard.js";
 import Quiz from "../models/Quiz.js";
 
